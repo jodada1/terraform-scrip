ba485d7932fc3eb476de162b371132763c6881b6
jodada1 <dadajohn03@gmail.com>
2022-04-19 23:32:42 -0700

    added terraform

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "my_image" {
+  type    = string
+  default = "ami-0fb653ca2d3203ac1"
+}
+
+variable "my_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "my_tag" {
+  type    = string
+  default = "jodada_terra"
+}
